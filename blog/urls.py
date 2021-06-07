344ad3f878f2b28bec699102c7d1bf46bb58c2cd
ShinSeungMin <sky2197kr@gmail.com>
2021-06-07 17:31:37 +0900

    실습9

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,9 @@
 from django.conf.urls import url
+from django.urls import path 
 from . import views
 
 urlpatterns = [
   url(r'^$', views.post_list, name='post_list'),
+  # path('post/<int:pk>/', views.post_detail, name='post_detail'),
+  url(r'^post/(?P<pk>\d+)/$', views.post_detail, name="post_detail"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Post
+from django.shortcuts import render, get_object_or_404
+
 # Create your views here.
 def post_list(request):
     posts = Post.objects.order_by('published_date')
     print(Post.objects)
     return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+    print(pk)
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
